551cdca4b30ae8e75cdc3d8f047d52c532e8aa81
andrezada <serrimanco@gmail.com>
2025-07-14 19:29:13 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,149 @@
+//Alunos:
+//André Luís de Moura Rodrigues
+//João Daniel Gomes dos Santos
+//Anna Júlya Alves Carvalho
+
+
+//Questão 1
+console.log(25+17)
+//soma
+
+//Questão 2
+console.log(250-100)
+//subtração 
+
+//Questão 3
+console.log(9*6)
+//multiplicação
+
+//Questão 4
+console.log(144/12)
+//divisão
+
+//questão 5
+console.log(55%6)
+//resto
+
+//Questão 6
+let x = 10 
+console.log(x++)
+console.log(x)
+//incrementar
+
+//Questão 7
+let y = 5
+console.log(y++)
+console.log(y++)
+console.log(y)
+// incrememtar 2 vezes
+
+//Questão 8
+let s = (10+5)
+let sub = (s-3)
+console.log(sub*2)
+//soma com 5 subtrair por 3 e multiplicar por 2
+
+//Questão 9
+let som = (8+7.5+10)
+console.log (som/3)
+//media
+
+//Questão 10 
+let a = 20
+let b = 5
+console.log(a+=b)
+console.log(a*=2)
+// soma multiplica e compara
+
+//Questão 11
+console.log (10>7)
+//verifique se e maior
+
+//Questão 12
+console.log(5<=3)
+//verifique se menor
+
+//Questão 13
+console.log(7=='7')
+console.log(7==='7')
+//verificar a igualdade
+
+//Questão 14
+console.log(10!=='10')
+console.log(10!='10')
+//negar a comparação
+
+//Questão 15
+console.log ((5 > 3) && (8 < 10))
+//e
+
+//Questão 16
+console.log((5 < 3) || (8 === 8)) 
+//ou
+
+//Questão 17
+console.log( !(7 > 2))
+//negação
+
+//Questão 18
+let som1 = ((4 + 3) * 2)
+let som2 = (4 + 3 * 2)
+console.log(som1===som2)
+//comparar as operações
+
+//Questão 19
+console.log(2**4)
+//potenciação
+
+//Questão 20 
+console.log(((10 + 2) / 2) ** 2)
+//soma divisão e potenciação
+
+//Questão 21
+let com1 = (4+4)
+let com2 = (2*4)
+console.log(com1===com2)
+//verifique as operação
+
+//Questão 22
+let t1 = (10==11)
+let t2 = (20==20)
+console.log(t1&&t2)
+//operação com 3 menbros false
+
+//Questão 23
+let h = (10==11)
+let jup = !(h)
+console.log(!(h))
+//operação com 3 menbros true
+
+//Questão 24
+console.log(5 + 3 * 2 ** 2)
+//soma multiplica e potencia
+
+//Questão 25
+let r = 3; r += 2 * 4;
+console.log(r /= 2)
+
+
+//Questão 26
+let nome = "Ana"
+let sobrenome = "Silva"
+console.log(nome, sobrenome) 
+//juntar as string
+
+//Questão 27
+console.log(("5" + 5) == 55)
+//junta uma string com um numero e compara
+
+//Questão 28 
+console.log( true && false || true)
+//testar true e false depois o resultado ou true
+
+//Questão 29
+console.log((3 * 4 + 2 === 14) && (10 > 5) || (7 < 2))
+//expressão com operadores que resulte em true
+
+//Questão 30
+console.log((10 + 5 * 2) > (30 / 2) && !false)
+//efetua as operações dentro dos parenteses, avalia se a primeira e maior e depois compara utilizando "e" com a negação de false 
